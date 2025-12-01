a504a05492566ebe30c932a5958a0d4345e0fcc7
AbhayFulara <abhayfulara@gmail.com>
2025-12-01 20:15:18 +0530

    Delete HospitalManagement/HospitalManagementSystem directory

--- HospitalManagement/HospitalManagementSystem/src/HospitalManagementSystem.java
+++
@@ -1,8 +0,0 @@
-public class HospitalManagementSystem {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            ui.MainFrame frame = new ui.MainFrame();
-            frame.setVisible(true);
-        });
-    }
-}
--- HospitalManagement/HospitalManagementSystem/src/util/DBException.java
+++
@@ -1,6 +0,0 @@
-package util;
-
-public class DBException extends Exception {
-    public DBException(String msg) { super(msg); }
-    public DBException(String msg, Throwable t) { super(msg, t); }
-}
